cd255e7e48c0e7b93920d032d21c4ade68384e3f
gaia <gayathiri.ravendirane@gmail.com>
2026-03-18 21:50:40 +0100

    chess main implemented

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 // ==============================================================================
 
 #include <iostream>
+#include <sstream>
+#include <string>
 
 #include "checkerboard.hpp"
 
@@ -24,17 +26,61 @@ White move:
 > D3
 */
 
+
+void chessMain() {
+  Checkerboard cb;
+  cb.initialConditions();
+
+  std::string input;
+  bool turnBlack = false; // white starts
+
+  while (true) {
+      std::cout << cb.toString() << std::endl;
+      std::cout << (turnBlack ? "Black" : "White") << " move: \n> ";
+      std::getline(std::cin, input);
+
+      if (input == "quit") {
+          break;
+      }
+
+      std::istringstream iss(input);
+      std::string startStr, endStr;
+
+      if (!(iss >> startStr >> endStr)) {
+          std::cout << "Invalid format. Use: `D6 D4`\n";
+          continue;
+      }
+
+      try {
+          Position start(startStr);
+          Position end(endStr);
+
+          cb.play(start, end, turnBlack);
+          turnBlack = !turnBlack; // switch player
+          // spacing from the previous player
+          for(int i = 0; i < 3; ++i) {
+              std::cout << "\n";
+          }
+      }
+      catch (const std::exception& e) {
+          std::cout << "Invalid move: " << e.what() << std::endl;
+      }
+  }
+}
+
 int main() {
   std::cout << "+==============================================================+" << std::endl;
   std::cout << "|                       CHESS & CHECKERS                       |" << std::endl;
   std::cout << "+==============================================================+" << std::endl;
 
-  Checkerboard cb;
+  chessMain();
 
-  cb.initialConditions();
+  // Checkerboard cb;
 
-  std::cout << cb.toString() << std::endl;
+  // cb.initialConditions();
 
+  // std::cout << cb.toString() << std::endl;
+  
 
   return 0;
 }
--- src/pawn.cpp
+++ src/pawn.cpp
@@ -4,6 +4,29 @@ Pawn::Pawn(bool isBlack):Piece(isBlack, "pawn") {}
 
 bool Pawn::isValidMove(const Position &start_pos, const Position &end_pos, 
     bool isCapture, Plateau* board) const {
-    // TODO
-    return true;
+    (void)board;
+
+    const int direction = getIsBlack() ? 1 : -1;
+    const int dx = end_pos.getX() - start_pos.getX();
+    const int dy = end_pos.getY() - start_pos.getY();
+    const int startRow = getIsBlack() ? 2 : 7;
+
+    if (isCapture) {
+        return std::abs(dx) == 1 && dy == direction;
+    }
+
+    if (dx != 0) {
+        return false;
+    }
+
+    if (dy == direction) {
+        return true;
+    }
+
+    // first move 2 square advance from the starting row
+    if (start_pos.getY() == startRow && dy == 2 * direction && firstMove == true) {
+        return true;
+    }
+
+    return false;
 }
