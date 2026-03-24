a49c217afbe3dbee02d68b0d006fec8a35e8f929
gaia <gayathiri.ravendirane@gmail.com>
2026-03-24 18:06:45 +0100

    visual

--- src/checkerboard.cpp
+++ src/checkerboard.cpp
@@ -91,7 +91,7 @@ std::string Checkerboard::toUnicodeString() const {
     for (int x = 0; x <= getWidth(); x++) {
         for (int y = 0; y <= getHeight(); y++) {
             if (x == 0 && y == 0) {
-                oss << " ";
+                oss << "  ";
             }
             else if (x != 0 && y == 0) {
                 oss << std::to_string(x) << " ";
